0214748e40e6f797abd4ba2b2b60dbaf4b5c3aac
RT <13197246+slikie@users.noreply.github.com>
2023-04-27 17:20:16 +0800

    init 3.4

---
+++ 3.4/builder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <GitHub repository> <Docker Hub repository>"
+  exit 1
+fi
+
+GITHUB_REPO=$1
+DOCKERHUB_REPO=$2
+
+git clone https://github.com/$GITHUB_REPO.git
+#no slashes prolly
+cd $(echo $GITHUB_REPO | cut -d'/' -f2)
+
+docker build -t $DOCKERHUB_REPO .
+
+docker login
+
+docker push $DOCKERHUB_REPO
